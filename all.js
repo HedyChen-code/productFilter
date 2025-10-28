4977837f1cd97ff6868d847063273a28ed3f5059
jiamori <lifesunny719@gmail.com>
2025-10-28 23:44:44 +0800

    加入撈取資料功能，主線六

--- all.js
+++ all.js
@@ -1,37 +1,6 @@
 console.clear();
 
-let data = [
-  {
-    "id": 0,
-    "name": "肥宅心碎賞櫻3日",
-    "imgUrl": "https://images.unsplash.com/photo-1522383225653-ed111181a951?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1655&q=80",
-    "area": "高雄",
-    "description": "賞櫻花最佳去處。肥宅不得不去的超讚景點！",
-    "group": 87,
-    "price": 1400,
-    "rate": 10
-  },
-  {
-    "id": 1,
-    "name": "貓空纜車雙程票",
-    "imgUrl": "https://images.unsplash.com/photo-1501393152198-34b240415948?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
-    "area": "台北",
-    "description": "乘坐以透明強化玻璃為地板的「貓纜之眼」水晶車廂，享受騰雲駕霧遨遊天際之感",
-    "group": 99,
-    "price": 240,
-    "rate": 2
-  },
-  {
-    "id": 2,
-    "name": "台中谷關溫泉會1日",
-    "imgUrl": "https://images.unsplash.com/photo-1535530992830-e25d07cfa780?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
-    "area": "台中",
-    "description": "全館客房均提供谷關無色無味之優質碳酸原湯，並取用八仙山之山冷泉供蒞臨貴賓沐浴及飲水使用。",
-    "group": 20,
-    "price": 1765,
-    "rate": 7
-  }
-];
+let data = [];
 
 // 定錨 dom 節點
 const ticketCardArea = document.querySelector('.ticketCard-area');
@@ -49,6 +18,9 @@ const addTicketBtn = document.querySelector('.addTicket-btn');
 
 const addTicketForm = document.querySelector('.addTicket-form');
 const cantFindArea = document.querySelector('.cantFind-area');
+
+// get 請求的資料路徑
+const url = 'https://raw.githubusercontent.com/hexschool/js-training/main/travelApi.json';
 
 // addTicket 綁定監聽 -> 新增套票功能
 addTicketBtn.addEventListener('click',function(){
@@ -136,4 +108,29 @@ function renderTickets(tickets){
   searchResultText.textContent = `本次搜尋共 ${tickets.length} 筆資料`;
 }
 
-renderTickets(data);
+
+// 撈取資料並渲染畫面
+
+async function getData() {
+  try {
+    const response = await axios.get(url);
+    data = response.data.data;
+    renderTickets(data);
+  }
+  catch (error) {
+    console.log('發生錯誤，回傳失敗')
+  }
+};
+
+// function getData(){
+//   axios.get(url)
+//   .then(function(response){
+//     data = response.data.data;
+//     renderTickets(data);
+//   })
+//   .catch(function(error){
+//     console.log('發生錯誤，回傳失敗')
+//   })
+// }
+
+getData();
